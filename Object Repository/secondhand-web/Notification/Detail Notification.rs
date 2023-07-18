94612fe24af2cb240f74e1f203009cc8642c0e04
nurasiyahh <nurasiyah727@gmail.com>
2023-07-18 21:19:40 +0700

    Initial commit

---
+++ Object Repository/secondhand-web/Notification/Detail Notification.rs
@@ -0,0 +1,312 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Detail Notification</name>
+   <tag></tag>
+   <elementGuidId>25b75e1e-60b1-45d5-a1b7-26b3966b130c</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Info Penawar'])[1]/following::section[2]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>section.container-sm</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>section</value>
+      <webElementGuid>1ebab9b6-a1f8-426c-8082-f9e9721b5ff3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>container-sm</value>
+      <webElementGuid>2b77ebb1-9445-4839-ba77-5f1cc0deb0f3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+  
+    
+      
+
+      
+        Abdillah Faisal
+        Solo
+      
+    
+  
+
+  Daftar Produkmu yang Ditawar
+
+  
+      
+        
+        
+          
+            
+              Penawaran produk
+            
+            
+              24 May, 14:21
+            
+          
+          
+            
+              Mukena 
+            
+            
+              Rp 100.000
+            
+            
+              Ditawar Rp -110.000
+            
+          
+          
+              
+                
+                Tolak
+
+                
+                Terima
+          
+        
+      
+
+      
+        
+          
+            
+              
+            
+
+            
+              Perbarui status penjualan produkmu
+              
+                
+                
+                  Berhasil terjual
+                Kamu telah sepakat menjual produk ini kepada pembeli
+              
+              
+                
+                
+                  Batalkan transaksi
+                Kamu membatalkan transaksi produk ini dengan pembeli
+              
+            
+
+            
+              
+            
+        
+      
+
+        
+  
+</value>
+      <webElementGuid>172884ec-ea40-4b32-a995-27233a6edd37</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/section[@class=&quot;pt-5 mt-5&quot;]/section[@class=&quot;container-sm&quot;]</value>
+      <webElementGuid>3ca0c84b-67c2-46c7-82ec-9cd00efdaf2c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Info Penawar'])[1]/following::section[2]</value>
+      <webElementGuid>627951c1-b154-4d98-b9b3-80dc2590a4ad</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/section</value>
+      <webElementGuid>87e165de-ece3-480a-a6b2-0b40fd68ac0d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//section[(text() = '
+  
+    
+      
+
+      
+        Abdillah Faisal
+        Solo
+      
+    
+  
+
+  Daftar Produkmu yang Ditawar
+
+  
+      
+        
+        
+          
+            
+              Penawaran produk
+            
+            
+              24 May, 14:21
+            
+          
+          
+            
+              Mukena 
+            
+            
+              Rp 100.000
+            
+            
+              Ditawar Rp -110.000
+            
+          
+          
+              
+                
+                Tolak
+
+                
+                Terima
+          
+        
+      
+
+      
+        
+          
+            
+              
+            
+
+            
+              Perbarui status penjualan produkmu
+              
+                
+                
+                  Berhasil terjual
+                Kamu telah sepakat menjual produk ini kepada pembeli
+              
+              
+                
+                
+                  Batalkan transaksi
+                Kamu membatalkan transaksi produk ini dengan pembeli
+              
+            
+
+            
+              
+            
+        
+      
+
+        
+  
+' or . = '
+  
+    
+      
+
+      
+        Abdillah Faisal
+        Solo
+      
+    
+  
+
+  Daftar Produkmu yang Ditawar
+
+  
+      
+        
+        
+          
+            
+              Penawaran produk
+            
+            
+              24 May, 14:21
+            
+          
+          
+            
+              Mukena 
+            
+            
+              Rp 100.000
+            
+            
+              Ditawar Rp -110.000
+            
+          
+          
+              
+                
+                Tolak
+
+                
+                Terima
+          
+        
+      
+
+      
+        
+          
+            
+              
+            
+
+            
+              Perbarui status penjualan produkmu
+              
+                
+                
+                  Berhasil terjual
+                Kamu telah sepakat menjual produk ini kepada pembeli
+              
+              
+                
+                
+                  Batalkan transaksi
+                Kamu membatalkan transaksi produk ini dengan pembeli
+              
+            
+
+            
+              
+            
+        
+      
+
+        
+  
+')]</value>
+      <webElementGuid>c270a0d0-8555-43e4-ba73-65793519b19a</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/secondhand-web/Notification/Notification Icon.rs
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Notification Icon</name>
+   <tag></tag>
+   <elementGuidId>7b35caeb-00c2-4e7a-be2b-2ff0013312c0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='navbarSupportedContent']/div/ul/li[3]/a/i</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>a.nav-link.dropdown-toggle.position-relative > i.bi.bi-bell</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+      <webElementGuid>c623fca3-5e06-49f6-b482-04be2dad61fe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>bi bi-bell</value>
+      <webElementGuid>0a44644e-2031-40ed-8f22-3d936985f7e1</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;navbarSupportedContent&quot;)/div[@class=&quot;ms-auto&quot;]/ul[@class=&quot;navbar-nav&quot;]/li[@class=&quot;nav-item dropdown me-0 me-lg-2 fs-5 d-none d-xl-block position-relative&quot;]/a[@class=&quot;nav-link dropdown-toggle position-relative&quot;]/i[@class=&quot;bi bi-bell&quot;]</value>
+      <webElementGuid>bf8ab50a-2f30-4552-88fd-5568f5db03b5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='navbarSupportedContent']/div/ul/li[3]/a/i</value>
+      <webElementGuid>58dafe4a-9fc3-47ed-bac4-d8e54b608044</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//li[3]/a/i</value>
+      <webElementGuid>bcd3011a-12be-4ade-a175-1ad832d3b4f0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
