e12cd473a18c1cba325491c99ed5ccafe0b32503
oktaviadpp <oktaviadwiputri506@gmail.com>
2023-07-24 12:57:51 +0700

    [qa/okta] Add report wed add, edit product and logout app

---
+++ Test Suites/secondhand-app/Feature Logout.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Feature Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b525dedc-910f-4cb5-92a3-66e5a372b6b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>4845b74e-a6f5-404a-8f42-c796968883b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/secondhand-app/Step Definition/Feature Logout/LGT001 - user should be able to logout from app</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
--- Test Suites/secondhand-web/Feature Add Product.ts
+++ Test Suites/secondhand-web/Feature Add Product.ts
@@ -12,14 +12,14 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>0341bc3f-b3a3-4422-a23a-4e5d5e0a0c57</testSuiteGuid>
    <testCaseLink>
-      <guid>59057872-ae29-4e09-a14a-1270e81dbc7b</guid>
+      <guid>7535cf94-7e26-44ff-86ac-e12b9df0fc9d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/secondhand-web/Step Definition/Feature Add Product/AddCat001 - user should be abe to add product catalog with valid input</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
-      <guid>c5474530-b983-450d-87ca-6f029c49f4af</guid>
+      <guid>7695cf27-e280-4b5d-9321-b4aef452166a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/secondhand-web/Step Definition/Feature Add Product/AddCat002 - user should not be abe to add product catalog with empty input price</testCaseId>
